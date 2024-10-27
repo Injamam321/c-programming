eadb1a7dde52cda040539631a55d3d4a02f831a6
Md Injamam Hossain <129589318+Injamam321@users.noreply.github.com>
2024-10-28 00:00:19 +0600

    Limitation of C Programming

---
+++ limitation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    // //double a=22.123456789123456;
+    // printf("%0.15lf",a);    
+
+
+    // int a=1000000000;
+    // printf("%d ",a);
+
+    long long int b=1000000000000000000;
+    printf("%lld",b);
+
+    return 0;
+}
